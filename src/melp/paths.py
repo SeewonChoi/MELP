08a9b7fe041729a10fb03be62b41d3319f485b47
Mayank Keoliya <keoliyamayank@gmail.com>
2025-09-06 10:49:54 -0400

    change dir

--- src/melp/paths.py
+++ src/melp/paths.py
@@ -3,7 +3,9 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).resolve().parents[2]
 # TODO: configure this path
 # RAW_DATA_PATH = "Your dataset path here"
-RAW_DATA_PATH = "/home/seewon/MELP/data/physionet.org/files/ptb-xl/1.0.3"
+RAW_DATA_PATH = "/home/mkeoliya/common-data/arpa-h/ca"
+PROCESSED_DATA_PATH = "/home/mkeoliya/MELP/data/"
+OTHER_DATA_PATH = "/home/mkeoliya/common-data/arpa-h/ca"
 SPLIT_DIR = ROOT_PATH / "src/melp/data_split"
 PROMPT_PATH = ROOT_PATH / "src/melp/prompt/CKEPE_prompt.json"
 DATASET_LABELS_PATH = ROOT_PATH / "src/melp/prompt/dataset_class_names.json"
